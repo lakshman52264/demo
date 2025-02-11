659b479dbd09e7c6eb527fc76f2bbcca0ad344bd
lakshman52264 <144992465+lakshman52264@users.noreply.github.com>
2025-02-11 10:15:56 +0530

    fixed division by zerp

--- calc.py
+++ calc.py
@@ -21,6 +21,8 @@ class calculator:
     def mul(self, a, b):
         return a * b
     def div(self, a, b):
+        if b == 0:
+            return "Error: Division by zero is not allowed."
         return a / b
     def power(self, a, b):
         return a ** b
